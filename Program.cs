64e57a320ea37321f501ae08e5590be5267c4543
darialukianenko2008-stack <daria.lukianenko2008@gmail.com>
2025-09-04 20:37:13 +0300

    added numbers

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("My name Daria");
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FeezBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Feez");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
